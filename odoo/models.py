e01f61cd9b9860eae774b6c9f24fd2c5ee4ce26d
mreficent <miquel.raich@eficent.com>
2020-01-22 12:44:29 +0100

    [IMP] base: _table_has_rows -> _table_has_null_rows
    No need to get default value and initialize values for columns that don't contain any null value.

--- odoo/models.py
+++ odoo/models.py
@@ -2539,11 +2539,11 @@ class BaseModel(MetaModel('DummyModel', (object,), {'_register': False})):
             self._cr.execute(query, (value,))
 
     @ormcache()
-    def _table_has_rows(self):
-        """ Return whether the model's table has rows. This method should only
-            be used when updating the database schema (:meth:`~._auto_init`).
+    def _table_has_null_rows(self, column_name):
+        """ Return whether the model's table has rows with null values in column_name.
+        This method should only be used when updating the database schema (:meth:`~._auto_init`).
         """
-        self.env.cr.execute('SELECT 1 FROM "%s" LIMIT 1' % self._table)
+        self.env.cr.execute('SELECT 1 FROM "%s" WHERE "%s" IS NULL LIMIT 1' % (self._table, column_name))
         return self.env.cr.rowcount
 
     def _auto_init(self):
